6a45ad0c8a4b6e6b11cc376bb2edff98e8d74ea6
Marshall Bruner <brunerm99@gmail.com>
2020-02-06 13:25:59 -0700

    added sine function if phase div by pi/2

--- src/main/java/complexProject/TestSpace.java
+++ src/main/java/complexProject/TestSpace.java
@@ -2,9 +2,9 @@ package complexProject;
 
 public class TestSpace {
 	public static void main(String[] args) {
-		//complexNumber cm = new complexNumber(9, 3);
-		//complexSignal sig = new complexSignal(cm);
-		//System.out.println(cm);
+		complexNumber cm = new complexNumber(0, 3);
+		complexSignal sig = new complexSignal(cm);
+		System.out.println(sig.toTime());
 		//System.out.println(sig);
 	}
 }
--- src/main/java/complexProject/complexSignal.java
+++ src/main/java/complexProject/complexSignal.java
@@ -6,6 +6,7 @@ public class complexSignal {
 	
 	double phase;
 	double magnitude;
+	boolean divByPi2;
 
 	public complexSignal() {
 		
@@ -14,8 +15,16 @@ public class complexSignal {
 	public complexSignal(complexNumber cm) {
 		this.magnitude = Math.sqrt(Math.pow(cm.re, 2) + Math.pow(cm.im, 2));
 		if (cm.re != 0) this.phase = Math.atan(cm.im / cm.re);
-		else if (cm.re == 0 && cm.im != 0) this.phase = 90;
+		else if (cm.re == 0 && cm.im != 0) {
+			this.phase = 90 ;
+			this.divByPi2 = true;
+		}
 		else this.phase = 0;
+	}
+
+	public String toTime(){
+		if (divByPi2) return this.magnitude + "sin(wt + " + (this.phase % 				(Math.PI/2)) + ")"; 
+		return this.magnitude + "cos(wt + " + this.phase + ")";
 	}
 	
 	public String toString() {
